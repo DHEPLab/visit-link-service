ad60d80e9c8537a775b9f720bd54042b222a0610
Tang Zhen <cloudztang@gmail.com>
2024-08-30 19:57:05 +0800

    [HF-43][Zhen Tang]feat: use the default aws config instead of config them

--- src/main/java/edu/stanford/fsi/reap/config/AwsConfig.java
+++ src/main/java/edu/stanford/fsi/reap/config/AwsConfig.java
@@ -1,32 +1,16 @@
 package edu.stanford.fsi.reap.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsConfig {
-
-    @Value("${application.aws.access-key-id}")
-    private String accessKeyId;
-
-    @Value("${application.aws.secret-access-key}")
-    private String secretAccessKey;
-
-    @Value("${application.aws.region}")
-    private String region;
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
-                ))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
