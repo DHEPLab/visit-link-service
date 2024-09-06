278e73c4c97890fec674091203352dd86f212a43
Tang Zhen <cloudztang@gmail.com>
2024-09-06 13:30:02 +0800

    [HF-38][Zhen Tang]fix: correct the carer history name pattern

--- src/main/java/edu/stanford/fsi/reap/entity/CarerHistory.java
+++ src/main/java/edu/stanford/fsi/reap/entity/CarerHistory.java
@@ -23,7 +23,7 @@ public class CarerHistory extends AbstractHistoryEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @NotNull @Size(min = 2, max = 10)
+  @NotNull @Size(min = 1, max = 50)
   private String name;
 
   @NotNull @Size(max = 20)
