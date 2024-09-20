2d60b0dba5909746682a6826d0852357e413bbe8
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-09-20 16:31:38 +0800

    [HF-87][Zhenjia Zhou]fix: update existing data in carer and carer history table

---
+++ src/main/resources/db/migration/V20240920163000__update_carer_history_family_ties.sql
@@ -0,0 +1,9 @@
+UPDATE carer_history
+SET family_ties =
+        CASE
+            WHEN family_ties = 'GRANDMOTHER' THEN 'PATERNAL_GRANDMOTHER'
+            WHEN family_ties = 'GRANDFATHER' THEN 'MATERNAL_GRANDMOTHER'
+            WHEN family_ties = 'GRANDMA' THEN 'PATERNAL_GRANDFATHER'
+            WHEN family_ties = 'GRANDPA' THEN 'MATERNAL_GRANDFATHER'
+            ELSE family_ties
+            END;
