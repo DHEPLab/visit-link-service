c1851dfe487c5b2022fcfce0b3988dfeef6359bf
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-09-23 10:39:06 +0800

    [HF-87][Zhenjia Zhou]fix: fix wrong enum

--- src/main/java/edu/stanford/fsi/reap/entity/enumerations/FeedingPattern.java
+++ src/main/java/edu/stanford/fsi/reap/entity/enumerations/FeedingPattern.java
@@ -3,7 +3,7 @@ package edu.stanford.fsi.reap.entity.enumerations;
 public enum FeedingPattern {
   // 母乳喂养，奶粉喂养，母乳奶粉混合喂养，已终止母乳/奶粉喂养
   EXCLUSIVE_BREASTFEEDING,
-  FEEDING_PATTERN,
+  FORMULA_FEEDING,
   MIXED_BREAST_FORMULA_FEEDING,
   NO_BREAST_FORMULA_FEEDING
 }
